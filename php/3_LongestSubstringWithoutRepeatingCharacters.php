3ec1f087f6572a7ceb36e5866e8d17f9d0abe5c7
ChenYu Xiao <apple.iiiyu@gmail.com>
2019-08-01 15:47:15 +1200

    搞定第三题

--- php/3_LongestSubstringWithoutRepeatingCharacters.php
+++ php/3_LongestSubstringWithoutRepeatingCharacters.php
@@ -2,84 +2,57 @@
 
 class Solution {
     /**
-     * @param String $s
-     * @return Integer
-     */
+    * @param String $s
+    * @return Integer
+    */
     function lengthOfLongestSubstring($s) {
-        $begin = 0;
-        $compare = 1;
-        $maxString = '';
-        $maxLenght = 1;
-        $currentLenght = 1;
+            if (strlen($s) === 0) {
+                return 0;
+            }
+            $begin = 0;
+            $compare = 1;
+            $maxString = '';
+            $maxLenght = 1;
+            $currentLenght = 1;
 
-        while ($compare < strlen($s)) {
-            $maxString = substr($s, $begin, $compare);
-//            echo var_dump($maxString);
-            echo var_dump($compare);
-            $pos = strpos($maxString, $s[$compare]);
-            if ($pos === false) {
-                // 没有重复
-                if (strlen($maxString) > $maxLenght) {
-                    $maxLenght = strlen($maxString);
-                }
-            } else {
-                // 重复
-                $begin = $begin + $pos + 1;
-                if ($begin + 1 === $compare) {
-                    $begin += 1;
-                    $compare = $begin + 1;
-                } else {
-                    $compare = $compare - 1;
-                }
-//                echo var_dump($compare);
-                if ($compare === strlen($s)) {
+            while ($compare < strlen($s)) {
+                $maxString = substr($s, $begin, $currentLenght);
+                $pos = strpos($maxString, $s[$compare]);
+                if ($pos === false) {
+                    $compare++;
+                    $currentLenght++;
                     if (strlen($maxString) > $maxLenght) {
                         $maxLenght = strlen($maxString);
                     }
+                } else {
+                    // 出现重复，记录最大值
+                    if (strlen($maxString) > $maxLenght) {
+                        $maxLenght = strlen($maxString);
+                    }
+                    // 起始位置从重复的下一位开始
+                    $begin = $begin + $pos + 1;
+                    $currentLenght = $compare - $begin;
+                    // 如果起始位置跟比较位置挨着
+                    if ($begin === $compare) {
+                        $compare++;
+                        $currentLenght = 1;
+                    }
                 }
             }
-        $compare++;
-
-//        for ($compare = 1; $compare < strlen($s); $compare++) {
-//            $maxString = substr($s, $begin, $compare);
-////            echo var_dump($maxString);
-//            echo var_dump($compare);
-//            $pos = strpos($maxString, $s[$compare]);
-//            if ($pos === false) {
-//                // 没有重复
-//                if (strlen($maxString) > $maxLenght) {
-//                    $maxLenght = strlen($maxString);
-//                }
-//            } else {
-//                // 重复
-//                $begin = $begin + $pos + 1;
-//                if ($begin + 1 === $compare) {
-//                    $begin += 1;
-//                    $compare = $begin + 1;
-//                } else {
-//                    $compare = $compare - 1;
-//                }
-////                echo var_dump($compare);
-//                if ($compare === strlen($s)) {
-//                    if (strlen($maxString) > $maxLenght) {
-//                        $maxLenght = strlen($maxString);
-//                    }
-//                }
-//
-//            }
+            if ($currentLenght > $maxLenght) {
+                $maxLenght = $currentLenght;
+            }
+            return $maxLenght;
         }
-
-        return $maxLenght;
-    }
 }
 
 
 $solution = new Solution();
 
-$string = 'pwwkew';
+$string = ' ';
 
-//echo var_dump(substr($string, 0, 1));
-//echo var_dump(strlen($string));
+//echo var_dump(substr($string, 2, 3));
+echo var_dump(strlen(" "));
 
 $result =  $solution->lengthOfLongestSubstring($string);
 echo var_dump($result);
